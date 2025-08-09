2ebffcfe4f7e580eb05dc9e6ea8dd8445ade1eb1
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-08-08 22:17:48 -0300

    Column

--- app/src/main/java/carreiras/com/github/jetpackcompose/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/jetpackcompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -29,9 +30,11 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun Inicial(modifier: Modifier = Modifier) {
-        TextoCustomizado("Primeira função composable", modifier)
-        TextoCustomizado("Seja bem vindo", modifier)
-        TextoCustomizado("Olá usuário", modifier)
+        Column {
+            TextoCustomizado("Primeira função composable.", modifier)
+            TextoCustomizado("Seja bem vindo!", modifier)
+            TextoCustomizado("Olá usuário.", modifier)
+        }
     }
 
     @Composable
