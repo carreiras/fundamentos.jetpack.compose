1c2855a3d0c33289c7bbda5f5fe90dad8142af41
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-08-08 22:49:58 -0300

    Box

--- app/src/main/java/carreiras/com/github/jetpackcompose/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/jetpackcompose/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import carreiras.com.github.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Inicial(Modifier.padding(innerPadding))
+                    NotificationBage(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -32,7 +38,7 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun Inicial(modifier: Modifier = Modifier) {
-        Column (modifier = modifier.background(color=Color.Blue)) {
+        Column(modifier = modifier.background(color = Color.Blue)) {
             TextoCustomizado("Primeira função composable.")
             Row {
                 TextoCustomizado("Seja bem vindo!")
@@ -50,11 +56,39 @@ class MainActivity : ComponentActivity() {
         )
     }
 
+    @Composable
+    fun NotificationBage(modifier: Modifier = Modifier) {
+        Box(modifier = modifier.padding(16.dp), contentAlignment = Alignment.Center) {
+            Box(
+                modifier = Modifier
+                    .size(24.dp)
+                    .clip(CircleShape)
+                    .background(color = Color.Green)
+            )
+            Box(
+                modifier = Modifier
+                    .size(8.dp)
+                    .clip(CircleShape)
+                    .background(color = Color.Red)
+                    .align(Alignment.TopEnd)
+            )
+        }
+
+    }
+
     @Preview(showBackground = true)
     @Composable
     fun PreviewFuncaoInicial() {
         JetpackComposeTheme {
             Inicial()
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewNotificationBage() {
+        JetpackComposeTheme {
+            NotificationBage()
         }
     }
 }
