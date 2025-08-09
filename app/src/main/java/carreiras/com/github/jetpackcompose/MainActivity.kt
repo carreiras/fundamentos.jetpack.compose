1d52902373a9556566a77ce638d4f93e7ed7e6a5
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-08-08 23:18:59 -0300

    Layout Card Mensagem

--- app/src/main/java/carreiras/com/github/jetpackcompose/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/jetpackcompose/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import carreiras.com.github.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -30,7 +35,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NotificationBage(Modifier.padding(innerPadding))
+                    CardMensagem(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -73,7 +78,27 @@ class MainActivity : ComponentActivity() {
                     .align(Alignment.TopEnd)
             )
         }
+    }
 
+    @Composable
+    fun CardMensagem(modifier: Modifier = Modifier) {
+        Row (modifier = modifier.padding(16.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.avatar),
+                contentDescription = "Foto do contato",
+                modifier = Modifier
+                    .size(52.dp)
+                    .clip(CircleShape)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(text = "Autor da mensagem")
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "Conteúdo da mensagem")
+            }
+        }
     }
 
     @Preview(showBackground = true)
@@ -89,6 +114,14 @@ class MainActivity : ComponentActivity() {
     fun PreviewNotificationBage() {
         JetpackComposeTheme {
             NotificationBage()
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewCardMensagem() {
+        JetpackComposeTheme {
+            CardMensagem()
         }
     }
 }
