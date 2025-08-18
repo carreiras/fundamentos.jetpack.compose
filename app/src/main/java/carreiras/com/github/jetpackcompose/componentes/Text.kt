dfeb7f6f35e88c04aee7264029d4d92d929fc5f0
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-08-18 19:31:16 -0300

    Máximo de linhas e overflow Elipsis

--- app/src/main/java/carreiras/com/github/jetpackcompose/componentes/Text.kt
+++ app/src/main/java/carreiras/com/github/jetpackcompose/componentes/Text.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -75,10 +76,29 @@ fun DifferentFont() {
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        Text(text = "Edu NSWACT Cursive regular", fontFamily = fontFamilyEDU, fontWeight = FontWeight.Normal)
-        Text(text = "Edu NSWACT Cursive medium", fontFamily = fontFamilyEDU, fontWeight = FontWeight.Medium)
-        Text(text = "Edu NSWACT Cursive semibold", fontFamily = fontFamilyEDU, fontWeight = FontWeight.SemiBold)
-        Text(text = "Edu NSWACT Cursive bold", fontFamily = fontFamilyEDU, fontWeight = FontWeight.Bold)
+        Text(
+            text = "Edu NSWACT Cursive regular. Eu preciso colocar um texto gigante neste local!",
+            fontFamily = fontFamilyEDU,
+            fontWeight = FontWeight.Normal,
+            maxLines = 1
+        )
+        Text(
+            text = "Edu NSWACT Cursive medium. Eu preciso colocar outro texto gigante neste local!",
+            fontFamily = fontFamilyEDU,
+            fontWeight = FontWeight.Medium,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = "Edu NSWACT Cursive semibold",
+            fontFamily = fontFamilyEDU,
+            fontWeight = FontWeight.SemiBold
+        )
+        Text(
+            text = "Edu NSWACT Cursive bold",
+            fontFamily = fontFamilyEDU,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
