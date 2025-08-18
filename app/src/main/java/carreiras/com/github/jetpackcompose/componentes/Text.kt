e9010cb07fb6e1feddba130dfea3799a5a20b76f
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-08-18 19:21:42 -0300

    Trabalhando com fontes

--- app/src/main/java/carreiras/com/github/jetpackcompose/componentes/Text.kt
+++ app/src/main/java/carreiras/com/github/jetpackcompose/componentes/Text.kt
@@ -2,8 +2,11 @@ package carreiras.com.github.jetpackcompose.componentes
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,10 +15,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import carreiras.com.github.jetpackcompose.R
 
@@ -54,9 +60,32 @@ fun TextShadow() {
 }
 
 
+@Composable
+fun DifferentFont() {
+    val fontFamilyEDU = FontFamily(
+        Font(R.font.edu_nswact_cursive_regular, FontWeight.Normal),
+        Font(R.font.edu_nswact_cursive_medium, FontWeight.Medium),
+        Font(R.font.edu_nswact_cursive_semibold, FontWeight.SemiBold),
+        Font(R.font.edu_nswact_cursive_bold, FontWeight.Bold),
+    )
+
+    Column {
+        Text(text = stringResource(R.string.hello_turma), fontFamily = FontFamily.Serif)
+        Text(text = stringResource(R.string.hello_turma), fontFamily = FontFamily.Monospace)
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(text = "Edu NSWACT Cursive regular", fontFamily = fontFamilyEDU, fontWeight = FontWeight.Normal)
+        Text(text = "Edu NSWACT Cursive medium", fontFamily = fontFamilyEDU, fontWeight = FontWeight.Medium)
+        Text(text = "Edu NSWACT Cursive semibold", fontFamily = fontFamilyEDU, fontWeight = FontWeight.SemiBold)
+        Text(text = "Edu NSWACT Cursive bold", fontFamily = fontFamilyEDU, fontWeight = FontWeight.Bold)
+    }
+}
+
 @Preview
 @Composable
 fun Preview() {
 //    SimpleText()
-    TextShadow()
+//    TextShadow()
+    DifferentFont()
 }
