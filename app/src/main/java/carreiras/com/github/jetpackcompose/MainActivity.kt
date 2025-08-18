a47e2fb5135b8a86229a7a7695c3330ddff54009
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-08-18 17:34:57 -0300

    Modifier - Parte 2

--- app/src/main/java/carreiras/com/github/jetpackcompose/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/jetpackcompose/MainActivity.kt
@@ -13,11 +13,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -107,8 +111,9 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun PreviewFuncaoInicial() {
         JetpackComposeTheme {
-//           Inicial()
-            TesteModifier()
+//            Inicial()
+//            TesteModifier()
+            TesteButtom()
         }
     }
 
@@ -147,13 +152,32 @@ class MainActivity : ComponentActivity() {
                     .padding(horizontal = 8.dp, vertical = 8.dp)
             )
 
+            Spacer(modifier = Modifier.height(8.dp))
+
             Text(
                 text = "Item 1",
                 modifier = Modifier
-                    .padding(horizontal = 8.dp)
                     .border(border = BorderStroke(width = 1.dp, color = Color.Green))
+                    .padding(horizontal = 8.dp)
             )
+        }
+    }
 
+
+    @Composable
+    fun TesteButtom() {
+        Button(
+            onClick = { /* TODO */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Green,
+                contentColor = Color.Black
+            ),
+            shape = RoundedCornerShape(4.dp)
+        ) {
+            Text(text = "Confirmar")
         }
 
     }
